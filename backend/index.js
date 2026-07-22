230125b3092fe83c1dc320e31d4c27effa941ddd
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-07-22 23:47:09 +0500

    Install express and setup basic template app

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
